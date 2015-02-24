71d21275c01e00e0ce3ba8de825b8717c56faf42
Jeremy Pereira <jeremyp@jeremyp.net>
2015-02-24 13:04:29 +0000

    Initial Commit

---
+++ EnigmaMachine/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  EnigmaMachine
+//
+//  Created by Jeremy Pereira on 24/02/2015.
+//  Copyright (c) 2015 Jeremy Pereira. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
