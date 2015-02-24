4feba539e3c23c7db00ce90fee93b2bc0568578d
Jeremy Pereira <jeremyp@jeremyp.net>
2015-02-24 13:13:16 +0000

    Skeleton enigma machine project

---
+++ components/Connection.swift
@@ -0,0 +1,12 @@
+//
+//  Connection.swift
+//  EnigmaMachine
+//
+//  Created by Jeremy Pereira on 24/02/2015.
+//  Copyright (c) 2015 Jeremy Pereira. All rights reserved.
+//
+
+import Foundation
+
+enum Letter {}
+
---
+++ components/Rotor.swift
@@ -0,0 +1,9 @@
+//
+//  Rotor.swift
+//  EnigmaMachine
+//
+//  Created by Jeremy Pereira on 24/02/2015.
+//  Copyright (c) 2015 Jeremy Pereira. All rights reserved.
+//
+
+import Foundation
